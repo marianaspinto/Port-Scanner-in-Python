052240f0b90c15469fc14df6eb3e1e8a9b5058df
Mariana Pinto <mariana.vendasbb@gmail.com>
2024-11-25 00:23:46 -0300

    Requesting IP/domain and defining ports

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
+# The Socket library allows establishing TCP or UDP connections,
+# which are essential for scanning ports on a IPadress or dominian.
 import socket
 
-print("Socket libary is imported successfully")
+# Request the IP address or the domain you wish to scan.
+alvo = input("Enter the IP address or domain to scan.")
+
+# Define a range of ports to scan.
+inicio_de_porta = int(input("Enter the starting port number"))
+fim_de_porta = int(input("Enter the final port number"))
