40ac91354b05c7d9e1ca9487f34a684066ebb690
Mariana Pinto <mariana.vendasbb@gmail.com>
2024-11-25 15:39:30 -0300

    code improvements

--- scanner_portas.py
+++ scanner_portas.py
@@ -23,13 +23,16 @@ def escanear_porta_tcp(ip, porta):
     try:
         # Criar um socket tipo TCP
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1) # Define um tempo limite de 1 segundo para cada tentativa 
+        sock.settimeout(2) # Define um tempo limite de 1 segundo para cada tentativa 
+        print(f"Tentando conectar à porta TCP {porta}...") # Diagnóstico
 
         # Tenta se conectar ao IP e porta especificados
         resultado = sock.connect_ex((ip, porta))
 
         if resultado == 0:
             print(f"Porta TCP {porta} está aberta.")
+        else:
+            print(f"Porta TCP {porta} está fechada.")
         sock.close()
     except socket.error as erro:
         print(f"Erro no TCP: {erro}")
@@ -50,10 +53,23 @@ def escanear_porta_udp(ip, porta):
         print(f"Erro no UDP:{erro}")
 
 # Função principal para iniciar o escaneamento com threads
-def escanear_tcp_com_threads(ip, portas, timeout):
-     for porta in portas:
-          thread =threading.Thread(target=escanear_porta_tcp, args=(ip, porta, timeout))
-          thread.start()        
+def iniciar_escaneamento_com_threads(ip, protocolo, inicio, fim):
+     threads = []
+     for porta in range(inicio, fim + 1):
+          if protocolo == "TCP":
+            thread = threading.Thread(target=escanear_porta_tcp, args=(ip, porta))
+          elif protocolo == "UDP":
+              thread = threading.Thread(target=escanear_porta_udp,args=(ip, porta))
+          threads.append(thread) # Adiciona a thread à lista
+          thread.start() # Inicia a thread
+
+# Aguardar todas as threads terminarem
+     for thread in threads:
+         thread.join()    
+
+# Chamar a função principal com as entradas do usuário
+print(f"\nIniciando escaneamento no alvo: {alvo}")
+iniciar_escaneamento_com_threads(alvo, protocolo, inicio_de_porta, fim_de_porta)
 
 
 
