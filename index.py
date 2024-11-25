2a2700be9fbd814f7a79ba8651651bfedd510f9f
Mariana Pinto <mariana.vendasbb@gmail.com>
2024-11-25 00:48:38 -0300

    Criando a função de escaneamento de portas

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
-# The Socket library allows establishing TCP or UDP connections,
-# which are essential for scanning ports on a IPadress or dominian.
+# A biblioteca Socket permite estabeleccer conexões TCP ou UDP,
+# que são essenciais para escanear portas em um endereço IP ou Domínio.
 import socket
 
-# Request the IP address or the domain you wish to scan.
-alvo = input("Enter the IP address or domain to scan.")
+# Solicitar IP ou Domínio que deseja escanear.
+alvo = input("Digite o endereço IP ou domínio para escanear.")
 
-# Define a range of ports to scan.
-inicio_de_porta = int(input("Enter the starting port number"))
-fim_de_porta = int(input("Enter the final port number"))
+# Definir um intervalo de portas.
+inicio_de_porta = int(input("Digite o número da porta incial."))
+fim_de_porta = int(input("Digite o núemro da porta final."))
+
+# Criando a função de escaneamento de portas
+def escanear_porta(ip, porta):
+    try:
+        # Criar um socket do tipo TCP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1) # Define um tempo limite de 1 segundo para cada tentativa 
+
+        # Tenta se conectar ao IP e porta especificados
+        resultado = sock.connect_ex((ip, porta))
+
+        if resultado == 0:
+            print(f"Porta {porta} está aberta.")
+        sock.close()
+    except socket.error as erro:
+        print(f"Não foi possível conectar ao servidor: {erro}")
+    except KeyboardInterrupt:
+        print("\nEscaneamento interrompido pelo usuário.")
+        exit()
+# 
